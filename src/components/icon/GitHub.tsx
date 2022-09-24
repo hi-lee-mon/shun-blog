f2c776c7d88638fc819b3a0abaea9cd3870c08bc
kameda <79642192+hi-lee-mon@users.noreply.github.com>
2022-09-25 04:02:25 +0900

    delete リモート上にだけ存在する不要ファイルを削除

--- src/components/icon/GitHub.tsx
+++
@@ -1,8 +0,0 @@
-import { Icon } from '@chakra-ui/react';
-import { BsGithub } from 'react-icons/bs';
-
-const GithubIcon = () => {
-  return <Icon as={BsGithub} title='Github icon' />;
-};
-
-export default GithubIcon;
