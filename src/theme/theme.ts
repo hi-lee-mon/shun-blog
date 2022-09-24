30868e7b57defaaf095978175a17369858bb4d65
shun <shunmel.09@gmail.com>
2022-09-25 02:06:39 +0900

    fix:背景色が誤っていたの修正

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,7 +3,7 @@ import { extendTheme } from '@chakra-ui/react';
 const styles = {
   global: () => ({
     body: {
-      // bg: '#202023',
+      bg: '#202023',
       color: '#DEDEDE',
     },
   }),
