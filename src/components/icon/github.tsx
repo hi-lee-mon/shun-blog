31cef942e112ae2c7a20d995cfab16e7afbd0b10
shun <shunmel.09@gmail.com>
2022-09-25 03:56:21 +0900

    ファイル名の大文字小文字修正

---
+++ src/components/icon/github.tsx
@@ -0,0 +1,8 @@
+import { Icon } from '@chakra-ui/react';
+import { BsGithub } from 'react-icons/bs';
+
+const GithubIcon = () => {
+  return <Icon as={BsGithub} title='Github icon' />;
+};
+
+export default GithubIcon;
